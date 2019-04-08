af29d44667a712b53dc69d56d921d561e78c3981
Rodrigo Castillo <rcastillo@hudsonhs.com>
2019-04-08 22:29:38 +0000

    dfsdfsds

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@
 /* global $ */
 
 $("#search-button").click(function(){
+ var here=    $("input").val(); 
+    $("#q4").click(function() { 
+$.ajax({
+    url: "https://api.giphy.com/v1/gifs/search?q="+here+"&rating=pg&api_key=dc6zaTOxFJmzC" , 
+    method : "GET" , 
+    success : function ( response ) { 
+        
+ 
+    
+
+console.log(dog);
+}
+
+});
   
   
   
